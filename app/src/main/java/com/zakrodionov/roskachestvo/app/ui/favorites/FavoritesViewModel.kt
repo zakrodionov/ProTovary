0cf0b7406007fe069ddb686ff1e495a67880a16b
zippo <zippo@mail.ru>
2019-03-13 16:09:13 +0300

    add toolbar

--- app/src/main/java/com/zakrodionov/roskachestvo/app/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/zakrodionov/roskachestvo/app/ui/favorites/FavoritesViewModel.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class FavoritesViewModel @Inject constructor(
-    getFavoriteProductsUseCase: GetFavoriteProductsUseCase,
+    val getFavoriteProductsUseCase: GetFavoriteProductsUseCase,
     val deleteFromStoresUseCase: DeleteFromStoreUseCase
 ) : BaseViewModel() {
 
