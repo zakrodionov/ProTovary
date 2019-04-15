256db6723d13bef1564a01d4af70f7a31c6e1276
Zakhar <zak.rodionov@ya.ru>
2019-04-15 10:56:32 +0300

    fix crash on meizu

---
+++ app/src/main/java/com/zakrodionov/protovary/app/ui/view/FixedTextInputEditText.kt
@@ -0,0 +1,40 @@
+package com.zakrodionov.protovary.app.ui.view
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+
+import java.lang.reflect.Field
+
+class FixedTextInputEditText : TextInputEditText {
+
+    private val superHintHack: CharSequence
+        @Throws(NoSuchFieldException::class, IllegalAccessException::class)
+        get() {
+            val hintField = TextView::class.java.getDeclaredField("mHint")
+            hintField.isAccessible = true
+            return hintField.get(this) as CharSequence
+        }
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun getHint(): CharSequence {
+        val manufacturer = Build.MANUFACTURER.toUpperCase()
+        return if (!manufacturer.contains("MEIZU") || Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            super.getHint()
+        } else {
+            try {
+                superHintHack
+            } catch (e: Exception) {
+                super.getHint()
+            }
+
+        }
+    }
+}
