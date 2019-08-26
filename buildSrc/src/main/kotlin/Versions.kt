48749e2264c175ecc5664a3f6ab09055739f7b09
zak1ro <zak.rodionov@ya.ru>
2019-08-26 20:48:36 +0300

    implementation kotlin gradle dsl

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,80 @@
+object Versions {
+    //Plugins
+    const val android_gradle_plugin = "3.5.0"
+    const val google_services_plugin = "4.0.1"
+    const val kotlin_gradle_plugin = "1.3.50"
+    const val fabric_plugin = "1.+"
+    const val gradle_versions_plugin = "0.22.0"
+    const val navigation_safe_args_plugin = "2.1.0-rc01"
+
+    //AndroidX
+    const val androidx_appcompat = "1.1.0-rc01"
+    const val androidx_constraint_layout = "2.0.0-beta2"
+    const val androidx_material = "1.1.0-alpha09"
+    const val androidx_cardview = "1.0.0"
+    const val androidx_recyclerview = "1.1.0-beta03"
+    const val ktx = "1.2.0-alpha03"
+    const val multidex = "2.0.1"
+    const val fragment = "1.1.0-alpha09"
+
+    //Kotlin
+    const val kotlin_coroutines = "1.3.0"
+
+    //Koin
+    const val koin = "2.0.1"
+
+    //Lifecycle
+    const val lifecycle = "2.2.0-alpha01"
+
+    //Room
+    const val room = "2.1.0"
+
+    //Networking
+    const val retrofit = "2.6.1"
+    const val okhttp_logging_interceptor = "3.12.0" //do not update, require android > 5.0
+    const val stetho = "1.5.1"
+
+    //Glide
+    const val glide = "4.9.0"
+
+    //Firebase
+    const val firebase_core = "17.1.0"
+    const val firebase_messaging = "20.0.0"
+
+    //Cicerone
+    const val cicerone = "5.0.0"
+
+    //PageIndicatorView
+    const val page_indicator_view = "1.0.3"
+
+    //CustomTabs
+    const val custom_tabs = "28.0.0"
+
+    //EventBus
+    const val event_bus = "3.1.1"
+
+    //Crashlytics
+    const val crashlytics = "2.10.1"
+
+    //SpongyCastle
+    const val spongycastle = "1.58.0.0"
+
+    //Anko
+    const val anko = "0.10.8"
+
+    //Navigation
+    const val navigation = "2.2.0-alpha01"
+
+    //Permission
+    const val permissions_dispatcher = "4.3.0"
+    //Barcode
+    const val barcode = "1.9.13"
+
+    //Test
+    const val junit = "4.12"
+    const val mockito_core = "2.2.28"
+    const val test_runner = "1.1.0"
+    const val test_rules = "1.1.0"
+    const val test_espresso = "3.1.1"
+    const val test_ext_junit = "1.1.0"
+}
