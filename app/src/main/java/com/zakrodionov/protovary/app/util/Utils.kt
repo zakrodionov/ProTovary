0044cf202abf7f1d80ec468e1f276ee0071a2997
zak_rodionov <zakrodionov@gmail.com>
2020-04-16 16:46:25 +0300

    clean up

--- app/src/main/java/com/zakrodionov/protovary/app/util/Utils.kt
+++ app/src/main/java/com/zakrodionov/protovary/app/util/Utils.kt
@@ -1,12 +1,5 @@
 package com.zakrodionov.protovary.app.util
 
-import android.view.Gravity
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.view.isVisible
-import androidx.transition.Slide
-import androidx.transition.TransitionManager
-
 /**
  * Created by Zakhar Rodionov on 20.03.19.
  */
@@ -21,21 +14,5 @@ object Utils {
             }
             else -> barcode
         }
-    }
-
-    fun slideTopTransition(view: View, animDuration: Long = 500) {
-        val transition = Slide(Gravity.BOTTOM)
-        transition.duration = animDuration
-        transition.addTarget(view.id)
-        TransitionManager.beginDelayedTransition(view.parent as ViewGroup, transition)
-        view.isVisible = false
-    }
-
-    fun slideBottomTransition(view: View, animDuration: Long = 500) {
-        val transition = Slide(Gravity.TOP)
-        transition.duration = animDuration
-        transition.addTarget(view.id)
-        TransitionManager.beginDelayedTransition(view.parent as ViewGroup, transition)
-        view.isVisible = true
     }
 }
