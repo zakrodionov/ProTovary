ee4cf63763b895e4fe137c239aeb2cb737081a87
Zak <zak.rodionov@ya.ru>
2021-03-29 11:44:16 +0300

    bump version code

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -4,7 +4,7 @@ object AndroidConfig {
     const val TARGET_SDK_VERSION = 30
     const val BUILD_TOOLS_VERSION = "30.0.0"
 
-    const val VERSION_CODE = 31
+    const val VERSION_CODE = 32
     const val VERSION_NAME = "1.16"
 
     const val APPLICATION_ID = "com.zakrodionov.protovary"
